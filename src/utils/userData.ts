1c446cc503876bb76dedd66f0143815675a6d62f
Gabriel Larangeira <gabriel.larangeira.silva@hotmail.com>
2023-04-04 09:25:20 -0300

    att infos

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Gabriel Larangeira",
+  githubUser: "GabrielLarangeira",
+  linkedinUser: "gabriel-larangeira",
+  whatsappNumber: "+5511966454999",
+  emailUser: "gabriel.larangeira.silva@hotmail.com",
+  instagramUser: "",
   facebookUser: "",
 };
